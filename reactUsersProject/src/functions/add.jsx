e05e36554b149e68ddf9eb880c299ce8a0429aa2
yakirahordes <yakirahordes@hilma.tech>
2024-11-14 10:58:13 +0200

    ..

---
+++ reactUsersProject/src/functions/add.jsx
@@ -0,0 +1,24 @@
+import apiRequest from "../components/apiRequest";
+import { API_URL } from "../functions/API_URL";
+
+export async function add(
+  e,
+  newObj,
+  sectionType,
+  setError,
+  setSection,
+  setAdd
+) {
+  e.preventDefault();
+  const randomId = Math.floor(Math.random() * (1000000 - 5000)) + 5000;
+  newObj.id = randomId.toString();
+  const postOption = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newObj),
+  };
+  const result = await apiRequest(`${API_URL}/${sectionType}`, postOption);
+  setError(result.errMsg);
+  setSection((prev) => [...prev, result.data]);
+  setAdd(false);
+}
--- reactUsersProject/src/pages/Todos.jsx
+++ reactUsersProject/src/pages/Todos.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { API_URL } from "../functions/API_URL";
 import Todo from "../components/Todo";
 import apiRequest from "../components/apiRequest";
@@ -16,7 +15,6 @@ export default function Todos() {
     isSearched: false,
     searchedTodos: [],
   });
-  const navigate = useNavigate();
   const userId = JSON.parse(localStorage.getItem("currentUserId"));
 
   useEffect(() => {
@@ -33,28 +31,33 @@ export default function Todos() {
     handleDelete(todosList, item, setTodosList, "todos", setError);
   }
 
-  console.log("todosList: ", todosList);
-
   async function addTodo(e) {
-    e.preventDefault();
-    const randonId = Math.floor(Math.random() * (1000000 - 200)) + 200;
+    // e.preventDefault();
+    // const randonId = Math.floor(Math.random() * (1000000 - 200)) + 200;
+    // const newTodo = {
+    //   id: randonId.toString(),
+    //   userId: parseInt(userId),
+    //   title: newTitle,
+    //   completed: false,
+    // };
+
+    // const postOption = {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify(newTodo),
+    // };
+    // const result = await apiRequest(`${API_URL}/todos`, postOption);
+    // setError(result.errMsg);
+    // setTodosList((prev) => [...prev, result.data]);
+    // setAdd(false);
     const newTodo = {
-      id: randonId.toString(),
       userId: parseInt(userId),
       title: newTitle,
       completed: false,
     };
-
-    const postOption = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newTodo),
-    };
-    const result = await apiRequest(`${API_URL}/todos`, postOption);
-    setError(result.errMsg);
-    setTodosList((prev) => [...prev, result.data]);
-    setAdd(false);
+    add(e, newTodo, "todos", setError, setTodos, setAdd);
   }
+
   function handleSearch(e) {
     setSearch(todosList);
     e.preventDefault();
@@ -94,25 +97,25 @@ export default function Todos() {
       <main className="todos-container">
         {!search.isSearched
           ? todosList.map((item) => {
-            return (
-              <Todo
-                key={item.id}
-                item={item}
-                deleteItem={deleteItem}
-                setError={setError}
-              />
-            );
-          })
+              return (
+                <Todo
+                  key={item.id}
+                  item={item}
+                  deleteItem={deleteItem}
+                  setError={setError}
+                />
+              );
+            })
           : search.searchedTodos.map((item) => {
-            return (
-              <Todo
-                key={item.id + "b"}
-                item={item}
-                deleteItem={deleteItem}
-                setError={setError}
-              />
-            );
-          })}
+              return (
+                <Todo
+                  key={item.id + "b"}
+                  item={item}
+                  deleteItem={deleteItem}
+                  setError={setError}
+                />
+              );
+            })}
       </main>
     </>
   );
